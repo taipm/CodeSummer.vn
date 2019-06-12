17675d3fdc2ec7f172b68768b1c6302f64a3723c
Phan Minh Tai <taipm.vn@gmail.com>
2019-06-12 09:47:01 +0700

    Clone from SachNoiHay.Club

---
+++ Web/ModelViews/BuyBookView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using CafeT.Text;
+
+namespace Web.ModelViews
+{
+    public class BuyBookView : BaseView
+    {
+        public string BookTilte { set; get; }
+        public string BookImageUrl { set; get; }
+        public string BuyBookUrl { set; get; }
+
+        public BuyBookView(string title, string url, string imageUrl)
+        {
+            BookTilte = title;
+            BookImageUrl = imageUrl;
+            BuyBookUrl = url;
+        }
+    }
+}
---
+++ Web/Models/ApplicationDbContext.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Json;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+
+namespace Web.Models
+{
+    public class ApplicationDbContext
+    {
+        private string ConnectionString = "https://spreadsheets.google.com/feeds/list/1QMLAbUgfLUtgZN4FCFY4aWc5eZFznXLTUEpKEhTPzOs/od6/public/values?alt=json";
+        public ArticleModel[] GetNews()
+        {
+            List<ArticleModel> books = new List<ArticleModel>();
+
+            using (WebClient httpClient = new WebClient())
+            {
+                httpClient.Encoding = Encoding.UTF8;
+
+                List<string> data = new List<string>();
+                var jsonData = httpClient.DownloadString(ConnectionString);
+
+                dynamic result = JsonValue.Parse(jsonData);
+                if (result.ContainsKey("feed"))
+                {
+                    dynamic feed = result["feed"];
+                    if (feed.ContainsKey("entry"))
+                    {
+                        dynamic entry = feed["entry"];
+
+                        for (int i = 0; i < entry.Count; i++)
+                        {
+                            ArticleModel book = Mappers.Mapper.EntryToArticle(entry[i]);
+
+                            books.Add(book);
+                        }
+                        return books.OrderByDescending(t => t.Id)
+                            .ToArray();
+                    }
+                }
+                return null;
+            }
+        }
+
+        public ArticleModel[] GetArticles()
+        {
+            List<ArticleModel> books = new List<ArticleModel>();
+
+            using (WebClient httpClient = new WebClient())
+            {
+                httpClient.Encoding = Encoding.UTF8;
+
+                List<string> data = new List<string>();
+                var jsonData = httpClient.DownloadString(ConnectionString);
+
+                dynamic result = JsonValue.Parse(jsonData);
+                if (result.ContainsKey("feed"))
+                {
+                    dynamic feed = result["feed"];
+                    if (feed.ContainsKey("entry"))
+                    {
+                        dynamic entry = feed["entry"];
+
+                        for (int i = 0; i < entry.Count; i++)
+                        {
+                            ArticleModel book = Mappers.Mapper.EntryToArticle(entry[i]);
+
+                            books.Add(book);
+                        }
+                        return books.OrderByDescending(t => t.Id)
+                            .ToArray();
+                    }
+                }
+                return null;
+            }
+        }
+
+        public ArticleModel GetArticleByContentUrl(string url)
+        {
+            var article = GetArticles().Where(t => t.ContentUrl == url).FirstOrDefault();
+            return article;
+        }
+
+        public ArticleModel GetArticleById(int id)
+        {
+            var article = GetArticles().Where(t => t.Id == id).FirstOrDefault();
+            return article;
+        }
+    }
+}
